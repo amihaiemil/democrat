f2252f918c26eb7f17005dc6911fa0a3f9e2bce8
amihaiemil <amihaiemil@gmail.com>
2018-12-23 11:44:27 +0200

    first interfaces

---
+++ democrat-jar/src/main/java/com/amihaiemil/democrat/Estimations.java
@@ -0,0 +1,7 @@
+package com.amihaiemil.democrat;
+
+/**
+ * Estimations in the system.
+ */
+public interface Estimations extends Iterable<Estimation> {
+}
---
+++ democrat-jar/src/main/java/com/amihaiemil/democrat/Votes.java
@@ -0,0 +1,7 @@
+package com.amihaiemil.democrat;
+
+/**
+ * Votes.
+ */
+public interface Votes extends Iterable<Vote> {
+}
